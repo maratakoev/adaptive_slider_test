821ca5d9cdc856b0848f7eff6f74f5032f0a86ba
AmalDoskhoev <dosxoevamal006@mail.ru>
2024-07-25 15:33:05 +0300

    Add pinia and .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  css: ["reset-css/reset.css", "~/assets/scss/main.scss"],
-  modules: ["shadcn-nuxt", "@nuxtjs/tailwindcss"],
-  shadcn: {
-    prefix: "Ui",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui-kit"
-     */
-    componentDir: "./components/ui-kit",
-  },
+	compatibilityDate: "2024-04-03",
+	devtools: {enabled: true},
+	css: ["reset-css/reset.css", "~/assets/scss/main.scss"],
+	modules: ["shadcn-nuxt", "@nuxtjs/tailwindcss", '@pinia/nuxt'],
+	shadcn: {
+		prefix: "Ui",
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui-kit"
+		 */
+		componentDir: "./components/ui-kit",
+	},
+	alias: {
+		// Этот alias нужен для корректной работы @pinia/nuxt, т.к. в текущей версии идет конфликт с npm и он начинает ругаться на зависимости
+		pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+	},
 });
