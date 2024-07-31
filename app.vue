dc9a3d776b177c428362ea1d313a8bac685bdaed
AmalDoskhoev <dosxoevamal006@mail.ru>
2024-07-31 17:37:17 +0300

    Add toast

--- app.vue
+++ app.vue
@@ -1,9 +1,26 @@
+<script
+	setup
+	lang="ts"
+>
+import Toaster from '@/components/ui-kit/toast/Toaster.vue';
+import { toast } from '~/components/ui-kit/toast';
+</script>
+
 <template>
 	<div>
 		<NuxtRouteAnnouncer />
 		<NuxtLayout>
 			<NuxtPage />
-			<UiButton>Я кнопка</UiButton>
+			<UiButton
+				@click="() => {
+					toast({
+						title: 'Тестовое уведомление',
+					});
+				}"
+			>
+				Показать уведомление
+			</UiButton>
 		</NuxtLayout>
+		<Toaster />
 	</div>
 </template>
